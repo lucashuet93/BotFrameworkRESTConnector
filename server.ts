a9b1b06ebd7343123dec0dc3fb8a88449ee0467c
lucashuet93 <lucashh@microsoft.com>
2017-07-06 12:51:48 -0700

    create initialize function to get access token for bot

--- server.ts
+++ server.ts
@@ -20,6 +20,27 @@ export class RESTConnectorBot {
         this.appId = appId;
         this.appId = appPassword;
     }
+
+    initialize = () => {
+        //sets the access token so the class can send and receive Activities from the connector
+        const tokenUrl = `https://login.microsoftonline.com/botframework.com/oauth2/v2.0/token`;
+        const tokenBody = `grant_type=client_credentials&client_id=${ProofOfConceptAppId}&client_secret=${ProofOfConceptAppPassword}&scope=https://api.botframework.com/.default`
+        const tokenConfig = {
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Host": "login.microsoftonline.com"
+            },
+        };
+        axios.post(tokenUrl, tokenBody, tokenConfig)
+            .then((res) => {
+                this.accessToken = res.data.access_token;
+                console.log(this.accessToken)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }
+
     listen = (req: Request, res: Response) => {
         console.log(req)
     }
@@ -32,6 +53,7 @@ export class RESTConnectorBot {
 let ProofOfConceptAppId: string = '5d3dc081-7572-4783-a385-e1a6f0afd179'
 let ProofOfConceptAppPassword: string = '0C5L7PkvFhDQz1VvxDJoPdT'
 let RESTBot = new RESTConnectorBot(ProofOfConceptAppId, ProofOfConceptAppPassword);
+RESTBot.initialize();
 
 //=========================================================
 // SET UP RESTIFY SERVER
