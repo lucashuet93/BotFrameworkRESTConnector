71629fbff5c8488f724bad5232ee77c2a9e75dd5
lucashuet93 <lucashh@microsoft.com>
2017-07-06 12:46:51 -0700

    instantiate class, create listen method

--- server.ts
+++ server.ts
@@ -7,14 +7,8 @@ import axios from 'axios';
 import { Request, Response, Server } from 'restify'
 
 //=========================================================
-// SETUP RESTIFY SERVER
+// REST CONNECTOR BOT
 //=========================================================
-
-let port: any = process.env.PORT || process.env.port || 3978;
-var server: Server = restify.createServer();
-server.listen(port, () => {
-    console.log(`Restify server running on ${server.url}`);
-});
 
 export class RESTConnectorBot {
 
@@ -25,5 +19,23 @@ export class RESTConnectorBot {
         this.appId = appId;
         this.appId = appPassword;
     }
-    
+    listen = (req: Request, res: Response) => {
+        console.log(req)
+    }
 }
+
+//=========================================================
+// SETUP RESTIFY SERVER
+//=========================================================
+
+let ProofOfConceptAppId: string = '5d3dc081-7572-4783-a385-e1a6f0afd179'
+let ProofOfConceptAppPassword: string = '0C5L7PkvFhDQz1VvxDJoPdT'
+let RESTBot = new RESTConnectorBot(ProofOfConceptAppId, ProofOfConceptAppPassword);
+
+let port: any = process.env.PORT || process.env.port || 3978;
+var server: Server = restify.createServer();
+server.use(restify.plugins.bodyParser());
+server.post('/api/messages', RESTBot.listen)
+server.listen(port, () => {
+    console.log(`Restify server running on ${server.url}`);
+});
